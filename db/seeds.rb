8472974d2f8aeb1ab57e816bf2c92c8fac6b8f2a
rrrht <salsasource31+services2@gmail.com>
2025-07-30 16:20:09 +0900

    Add Bootstrap via CDN and improve table layout

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+Player.create!(name: "Kodai NARAOKA", country: "JPN")
+Player.create!(name: "Viktor AXELSEN", country: "DK")
+Player.create!(name: "SHI Yu Qi", country: "CN")
+
+Tournament.create!(name: "Japan Open", grade: "Super 750", start_date: Date.today - 10.days)
+Tournament.create!(name: "England Open", grade: "Super 1000", start_date: Date.today - 40.days)
+
+Result.create!(player: Player.find_by(name: "Kodai NARAOKA"), tournament: Tournament.find_by(name: "Japan Open"), round: "QF" , points: 5000)
+Result.create!(player: Player.find_by(name: "Viktor AXELSEN"), tournament: Tournament.find_by(name: "Japan Open"), round: "SF" , points: 7000)
+Result.create!(player: Player.find_by(name: "SHI Yu Qi"), tournament: Tournament.find_by(name: "Japan Open"), round: "F" , points: 9000)
+
+Result.create!(player: Player.find_by(name: "Kodai NARAOKA"), tournament: Tournament.find_by(name: "England Open"), round: "R16" , points: 3000)
+Result.create!(player: Player.find_by(name: "Viktor AXELSEN"), tournament: Tournament.find_by(name: "England Open"), round: "QF" , points: 5000)
+Result.create!(player: Player.find_by(name: "SHI Yu Qi"), tournament: Tournament.find_by(name: "England Open"), round: "F" , points: 9000)
